84147d8afba4f63e0dbed31fcd629264185bbf6e
jpcc-93 <jpcc-93@hotmail.com>
2023-10-30 09:11:35 -0500

    Se crea repositorio en git, con el trabajo solo interfaz

---
+++ src/inenpoli/InEnpoli.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inenpoli;
+
+import GUI.Inicio;
+
+/**
+ *
+ * @author Pablo U
+ */
+public class InEnpoli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Se crea por medio de el paquete GUI las interfaces graficas
+        //Se instancia la ventana
+        Inicio inicio = new Inicio();
+         //se usa esta linea para que aparezca en el centro de la pantalla la aplicacion
+        inicio.setLocationRelativeTo(null);
+        //Se muestra la ventana
+        inicio.setVisible(true);
+    }
+    
+}
